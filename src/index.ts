08e671cd9d982a07a3dbfac6588842884b51e07c
Ido <gershido@gmail.com>
2023-08-17 16:38:22 +0300

    structure change

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { eligibilityModules, eligibilityFactory } from "./eligibility/index";
-import type {
-  EligibilityModule,
-  EligibilityFactory,
-} from "./eligibility/index";
-
-export { eligibilityModules, eligibilityFactory };
-
-export type { EligibilityModule, EligibilityFactory };
