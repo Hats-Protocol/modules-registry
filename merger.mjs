2bf173509411d5160d50f574eb5f8c4946b71517
Ido <gershido@gmail.com>
2023-09-05 13:19:41 +0300

    add chaining modules and change final modules file structure

--- merger.mjs
+++ merger.mjs
@@ -1,18 +1,31 @@
 import * as fs from "fs";
 
 export function mergeJson() {
-  const final = [];
+  const final = {
+    factory: undefined,
+    eligibilitiesChain: undefined,
+    togglesChain: undefined,
+    modules: [],
+  };
 
   const factoryBuffer = fs.readFileSync("./factory.json");
   const factory = JSON.parse(factoryBuffer);
-  final.push(factory);
+  final.factory = factory;
+
+  const eligibilitiesChainBuffer = fs.readFileSync("./eligibilitiesChain.json");
+  const eligibilitiesChain = JSON.parse(eligibilitiesChainBuffer);
+  final.eligibilitiesChain = eligibilitiesChain;
+
+  const togglesChainBuffer = fs.readFileSync("./togglesChain.json");
+  const togglesChain = JSON.parse(togglesChainBuffer);
+  final.togglesChain = togglesChain;
 
   let moduleFiles = fs.readdirSync("modules");
   for (let i = 0; i < moduleFiles.length; i++) {
     const moduleFile = moduleFiles[i];
     const moduleBuffer = fs.readFileSync(`./modules/${moduleFile}`);
     const module = JSON.parse(moduleBuffer);
-    final.push(module);
+    final.modules.push(module);
   }
 
   fs.writeFileSync("modules.json", JSON.stringify(final));
