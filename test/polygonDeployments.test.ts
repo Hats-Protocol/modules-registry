76fbe2b7c5f94c33ba918e300fc2bf7a43cc3c89
Ido <gershido@gmail.com>
2024-01-11 18:29:34 -0500

    wip

--- test/polygonDeployments.test.ts
+++ test/polygonDeployments.test.ts
@@ -1,4 +1,4 @@
-import {
+/*import {
   HatsModulesClient,
   solidityToTypescriptType,
 } from "@hatsprotocol/modules-sdk";
@@ -161,3 +161,4 @@ describe("Polygon deployments", () => {
     }
   }, 30000);
 });
+*/
